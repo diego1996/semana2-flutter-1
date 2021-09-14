2e6d074c69afeea6d2896ec563a2294061c7923c
GermanHerrera <germaneherrera@gmail.com>
2021-09-14 10:23:56 -0500

    Implementa estructura zonas

--- lib/pages/home.page.dart
+++ lib/pages/home.page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:semanda2flutter1/widgets/botones.widget.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key? key}) : super(key: key);
@@ -17,26 +18,88 @@ class _HomePageState extends State<HomePage> {
           Expanded(
             flex: 5,
             child: Container(
-              child: Text("zona resultados"),
+              child: Row(
+                children: [
+                  Text("zona resultados"),
+                ],
+              ),
               color: Colors.blue,
             ))
           ,
           Expanded(
             flex: 1,
             child: Container(
-              child: Text("zona ingreso"),
+              child: Row(
+                children: [
+                  Text("zona ingreso"),
+                ],
+              ),
               color: Colors.red,
             ))
             ,
-          Expanded(
+          Expanded( 
             flex: 2,
             child: Container(
-              child: Text("zona botones"),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: _listaBotonesFila1Prueba()),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: _listaBotonesFila1Prueba()),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: _listaBotonesFila1Prueba()),
+                 Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: _listaBotonesFila1Prueba()),
+                ],
+              ),
               color: Colors.green,
             ))
           ,
         ],
       ),
     );
+  }
+
+
+  _listaBotonesFila1Prueba() {
+    return [
+      pintarBoton(metodo: (){}, compoente:  Text("7"), colorBoton: Colors.yellow),
+      pintarBoton(metodo: (){}, compoente:  Icon(Icons.arrow_back_ios)),
+      pintarBoton(metodo: (){}, compoente:  Text("8")),
+      pintarBoton(metodo: (){}, compoente:  Text("9")),
+      pintarBoton(metodo: (){print("object");}, valorFlex: 4, compoente:  Text("=")),
+    ];
+  }
+
+  _listaBotonesFila1() {
+    return [
+      ElevatedButton(
+        onPressed: (){}, 
+        child: Text("7")
+      )
+      ,ElevatedButton(
+        onPressed: (){}, 
+        child: Text("8")
+      ),
+      ElevatedButton(
+        onPressed: (){}, 
+        child: Text("9")
+      ),
+      ElevatedButton(
+        onPressed: (){}, 
+        child: Text("÷")
+      ),
+      ElevatedButton(
+        onPressed: (){}, 
+        child: Icon(Icons.arrow_back)
+      ),
+      ElevatedButton(
+        onPressed: (){}, 
+        child: Text("C")
+      )];
   }
 }
---
+++ lib/widgets/botones.widget.dart
@@ -0,0 +1,19 @@
+ import 'package:flutter/material.dart';
+
+Widget pintarBoton({required dynamic metodo, int valorFlex = 1 , required Widget compoente, dynamic colorBoton = Colors.red }){
+    return Expanded(
+        flex: valorFlex,
+        child: Container(
+          child: 
+          Padding(
+            padding: const EdgeInsets.all(2.0),
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: colorBoton,
+              ),
+              onPressed: metodo, 
+              child: compoente
+            ),
+          ),
+      ));
+  }
